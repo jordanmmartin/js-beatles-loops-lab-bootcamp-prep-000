618f1f4677387cbd34d5216bd1e7ab4ff68aca6d
Jordan Martin <jdordan23@gmail.com>
2018-08-11 21:34:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ function theBeatlesPlay(musicians, instruments) {
   var array = []
   
   for (i = 0; i < musicians.length; i++) {
-    if(i === 0) {
       var string = musicians[i] + ' plays ' + instruments[i]
-      array.pop()
-    }else{
-      
-    }
+      array.pop(string)
   }
 }
